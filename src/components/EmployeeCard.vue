72c7d5f5ee73a76e294edaacd7ba675b577d9b58
ramyl <ramylayous@hotmail.com>
2024-05-28 15:07:58 +0200

    Create EmployeeCard component to display individual employee details
    - Added EmployeeCard component to show each employee in a card format
    - Displayed employee's avatar, full name, and contact link within the card
    - Created and imported EmployeeCard.css for styling the employee card

---
+++ src/components/EmployeeCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="employee-card">
+    <div class="profile-pic">
+      <img :src="employee.avatar" alt="Profile" />
+    </div>
+    <p>{{ fullName }}</p>
+    <a :href="`mailto:${employee.email}`">Contact</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EmployeeCard",
+  props: {
+    employee: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    fullName() {
+      return `${this.employee.first_name} ${this.employee.last_name}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import "@/Styles/EmployeeCard.css";
+</style>
