7736e9102105253ee4ad62ef5069e0a7d679a060
ramyl <ramylayous@hotmail.com>
2024-05-28 20:31:45 +0200

    Add pagination to EmployeeList and improve overall styling
    - Added pagination functionality to EmployeeList component
    - Created page and totalPages data properties in EmployeeList
    - Implemented methods for fetching employees based on the current page and navigating between pages
    - Created PaginationButton component for page navigation
    - Styled PaginationButton component with PaginationButton.css
    - Integrated PaginationButton in EmployeeList for better navigation
    - Enhanced styling of App.vue for improved user experience

--- src/components/EmployeeList.vue
+++ src/components/EmployeeList.vue
@@ -7,32 +7,64 @@
         :employee="employee"
       />
     </div>
+    <p class="page-number">{{ page }} / {{ totalPages }}</p>
+    <div class="pagination">
+      <PaginationButton @click="prevPage" :disabled="page === 1"
+        >Previous Page</PaginationButton
+      >
+      <PaginationButton @click="nextPage" :disabled="page === totalPages"
+        >Next Page</PaginationButton
+      >
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import EmployeeCard from "./EmployeeCard.vue";
+import EmployeeCard from "@/components/EmployeeCard.vue";
+import PaginationButton from "@/components/PaginationButton.vue";
 
 export default {
+  name: "EmployeeList",
   components: {
     EmployeeCard,
+    PaginationButton,
   },
   data() {
     return {
       employees: [],
+      page: 1,
+      totalPages: 1,
     };
   },
   created() {
     this.fetchEmployees();
   },
+  watch: {
+    page() {
+      this.fetchEmployees();
+    },
+  },
   methods: {
     async fetchEmployees() {
       try {
-        const response = await axios.get("https://reqres.in/api/users");
+        const response = await axios.get(
+          `https://reqres.in/api/users?page=${this.page}`
+        );
         this.employees = response.data.data;
+        this.totalPages = response.data.total_pages;
       } catch (error) {
         console.error("Error fetching employees:", error);
+      }
+    },
+    prevPage() {
+      if (this.page > 1) {
+        this.page--;
+      }
+    },
+    nextPage() {
+      if (this.page < this.totalPages) {
+        this.page++;
       }
     },
   },
@@ -40,5 +72,5 @@ export default {
 </script>
 
 <style scoped>
-@import '@/styles/EmployeeList.css';
+@import "@/styles/EmployeeList.css";
 </style>
---
+++ src/components/PaginationButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="onClick" :disabled="disabled">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "PaginationButton",
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    onClick() {
+      this.$emit("click");
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import "@/styles/PaginationButton.css";
+</style>
